bc06dd4115248e517373ccae4d1a343cbd5b735a
kjserbel <56279299+kjserbel@users.noreply.github.com>
2025-07-07 02:18:25 -0600

    Add files via upload

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import Dashboard from "../pages/Dashboard";
+import Scene3D from "../pages/Scene3D";
+import PrivateRoute from "./PrivateRoute";
 
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route element={<PrivateRoute />}>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/scene" element={<Scene3D />} />
+    </Route>
+  </Routes>
+);
+
+export default AppRoutes;
